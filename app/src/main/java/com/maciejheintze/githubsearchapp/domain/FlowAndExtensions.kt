b7ef6ff7dbb27d6584e698e41e8e0038679e1e7a
Maciek <maciej.heintze96@gmail.com>
2023-02-17 19:37:48 +0100

    flow extension functions implementation

--- app/src/main/java/com/maciejheintze/githubsearchapp/domain/FlowAndExtensions.kt
+++ app/src/main/java/com/maciejheintze/githubsearchapp/domain/FlowAndExtensions.kt
@@ -1,6 +1,27 @@
 package com.maciejheintze.githubsearchapp.domain
 
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
+import com.maciejheintze.githubsearchapp.base.BaseViewModel
+import com.maciejheintze.githubsearchapp.presentation.util.LoadingState
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.*
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.resumeWithException
 
 fun <T> flowSingle(action: suspend () -> T): Flow<T> = flow { emit(action.invoke()) }
+
+fun <T> Flow<T>.doToggleLoadingStateOf(
+    vm: BaseViewModel,
+): Flow<T> = this
+    .onStart { vm.isLoading.value = LoadingState(isLoading = true) }
+    .onCompletion { vm.isLoading.value = LoadingState(isLoading = false) }
+
+fun <T> Flow<T>.launchWith(
+    scope: CoroutineScope,
+    continuation: Continuation<Unit>? = null,
+    onError: ((Throwable) -> Unit),
+) = this
+    .catch {
+        continuation?.resumeWithException(it)
+        onError.invoke(it)
+    }
+    .launchIn(scope)
